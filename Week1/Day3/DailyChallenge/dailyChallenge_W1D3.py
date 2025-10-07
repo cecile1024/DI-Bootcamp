578c08edc2bc4c0c1ed3c4a7d364fb79691fa841
cecile1024 <cecile.eymond@gmail.com>
2025-10-07 18:09:01 +0200

    daily challenge W1D3 done

--- Week1/Day3/DailyChallenge/dailyChallenge_W1D3.py
+++ Week1/Day3/DailyChallenge/dailyChallenge_W1D3.py
@@ -20,18 +20,25 @@ class Farm():
     def get_animal_types(self):
          sorted_animals=sorted(self.animals.keys())
          print(f"{sorted_animals}")
-         return sorted_animals
+         return sorted_animals # list of animal type in alpha order
 
     def get_short_info(self):
-        animals_type=self.get_animal_types()
+        animals_type=self.get_animal_types() # return a list of animal type in alpha order
+        # mettre au pluriel les types d'animaux concernés
+
         for animal in animals_type:
-            if
-        print(f"{self.farm_name} has {' ,'.join(animals_type)}")
+            if self.animals[animal]>1:
+                index=animals_type.index(animal)
+                animals_type[index]+='s'
+        print(f"{self.farm_name} has {', '.join(animals_type[0:-1])} and {animals_type[-1]}")
+
+
 
 my_farm=Farm("Cecile's farm")
 my_farm.add_animal("cat",3)
+my_farm.add_animal("rabbit",5)
 my_farm.add_animal("dog",2)
-my_farm.add_animal("butterfly",100)
+my_farm.add_animal("duck",10)
 # print(f"{my_farm.animals}")
 my_farm.get_info()
 my_farm.get_animal_types()
